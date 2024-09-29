c7213e68ed9daf48692b8161487ccbd4078a5d15
Gustavo L de Mello (Guz) <contact@guz.one>
2024-09-29 14:30:29 -0300

    feat: add automatic garbage collection

--- configuration.nix
+++ configuration.nix
@@ -53,6 +53,11 @@
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.package = pkgs.nixVersions.nix_2_21;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 10d";
+  };
 
   networking = {
     networkmanager.enable = true;
