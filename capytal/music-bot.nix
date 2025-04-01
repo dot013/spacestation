85cbecc744ee36854acf885a723268a2b41a3043
Gustavo L de Mello (Guz) <contact@guz.one>
2025-04-01 10:09:01 -0300

    chore: disable muse bot temporarily

--- capytal/music-bot.nix
+++ capytal/music-bot.nix
@@ -1,13 +1,13 @@
 {config, ...}: {
-  imports = [];
-  virtualisation.oci-containers.containers.capytal-music = {
-    image = "codetheweb/muse:latest";
-    autoStart = true;
-    volumes = [
-      "/var/lib/muse/data:/data"
-    ];
-    environmentFiles = [
-      (/. + config.sops.secrets."discord/muse-bot/environment".path)
-    ];
-  };
+  # imports = [];
+  # virtualisation.oci-containers.containers.capytal-music = {
+  #   image = "ghcr.io/museofficial/muse:pr-1223";
+  #   autoStart = true;
+  #   volumes = [
+  #     "/var/lib/muse/data:/data"
+  #   ];
+  #   environmentFiles = [
+  #     (/. + config.sops.secrets."discord/muse-bot/environment".path)
+  #   ];
+  # };
 }
