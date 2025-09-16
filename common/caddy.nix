e3f146ddbaa25c5582b9f883ecbe91ae33561cb5
Gustavo "Guz" L de Mello <contact@guz.one>
2025-09-16 16:14:26 -0300

    feat: force nginx to be disabled

--- common/caddy.nix
+++ common/caddy.nix
@@ -1,6 +1,9 @@
 {...}: {
   services.caddy.enable = true;
 
+  # Some services try to enable nginx as their reverse proxy
+  services.nginx.enable = false;
+
   # Caddy Ports
   networking.firewall.allowedTCPPorts = [80 433];
 }
